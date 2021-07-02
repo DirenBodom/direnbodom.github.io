cfadb078bfe19304f1a4e95ac4dc651a2a2aa77e
Luis Chavez <luispazzo503@gmail.com>
2021-07-02 16:38:17 -0700

    Removed old site, replaced with new demo

--- _site/Song.py
+++
@@ -1,6 +0,0 @@
-class SongClass:
-    def __init__(self, artist, song, album, url: str = None):
-        self.artist = artist
-        self.song = song
-        self.album = album
-        self.image_url = url
